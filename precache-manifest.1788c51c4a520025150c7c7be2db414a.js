839581be1204a713f974e8bd542729dbefb39fb9
Sasha Savchuk <Sasha>
2019-12-23 01:21:11 +0200

    Updates

---
+++ precache-manifest.1788c51c4a520025150c7c7be2db414a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a3ffac7847c8b036669d6d223d5f78",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "96215390c9e04073ac05",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.70ad7286.chunk.css"
+  },
+  {
+    "revision": "c6d6130815cd84608b7e",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b39257a2.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b39257a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96215390c9e04073ac05",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.cee50520.chunk.js"
+  },
+  {
+    "revision": "72b540d1ba1a43e7b93f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.d9181fa4.js"
+  }
+]);
