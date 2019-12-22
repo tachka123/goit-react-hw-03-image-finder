db5edb4425eab201ab48da792da9bfa562a7414c
Sasha Savchuk <Sasha>
2019-12-23 01:48:25 +0200

    Updates

--- precache-manifest.29295bc56ef9dd902522aeaa264cf333.js
+++ precache-manifest.29295bc56ef9dd902522aeaa264cf333.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "61a3ffac7847c8b036669d6d223d5f78",
+    "revision": "1cd5a494ed35cfbdcdb2076633e66d3c",
     "url": "/goit-react-hw-03-image-finder/index.html"
   },
   {
-    "revision": "96215390c9e04073ac05",
+    "revision": "5227fdf48fa979a45b38",
     "url": "/goit-react-hw-03-image-finder/static/css/main.70ad7286.chunk.css"
   },
   {
-    "revision": "c6d6130815cd84608b7e",
-    "url": "/goit-react-hw-03-image-finder/static/js/2.b39257a2.chunk.js"
+    "revision": "f8d54e53f230cb6aaa72",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.bbbdaf6f.chunk.js"
   },
   {
     "revision": "c6d01cb19213691bc81a274434d00fe7",
-    "url": "/goit-react-hw-03-image-finder/static/js/2.b39257a2.chunk.js.LICENSE"
+    "url": "/goit-react-hw-03-image-finder/static/js/2.bbbdaf6f.chunk.js.LICENSE"
   },
   {
-    "revision": "96215390c9e04073ac05",
-    "url": "/goit-react-hw-03-image-finder/static/js/main.cee50520.chunk.js"
+    "revision": "5227fdf48fa979a45b38",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.c31b6eed.chunk.js"
   },
   {
     "revision": "72b540d1ba1a43e7b93f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.1788c51c4a520025150c7c7be2db414a.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.29295bc56ef9dd902522aeaa264cf333.js"
 );
 
 self.addEventListener('message', (event) => {
